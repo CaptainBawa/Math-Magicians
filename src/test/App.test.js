9700bb7110e4d1a7ac3e83ac9c4883a826777e1f
Collins Bawa <collinskwamebawa@yahoo.com>
2023-05-11 21:21:05 +0000

    Add test

--- src/test/App.test.js
+++ src/test/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import App from '../App';
 
-test('renders learn react link', () => {
+test('Math Magicians', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Math Magicians/i);
   expect(linkElement).toBeInTheDocument();
 });
